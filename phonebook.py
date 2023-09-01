81bb6bfcb9b98c0bd1306a3e3cf3dfbe3ed674f9
Naveen chand Gadde <139424272+ChanduG97@users.noreply.github.com>
2023-09-01 17:26:59 +0530

    Add files via upload

---
+++ phonebook.py
@@ -0,0 +1,4 @@
+mydict = {"Chandu":"9010148616","Loki":"8886818960","Marif":"9000567930"}
+print("The phonebook is \n -> ",mydict)
+mydict.pop("Loki")
+print("The phonebook after removing Loki is\n -> ",mydict)
